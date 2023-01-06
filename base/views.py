3ea55d4a0fba50dc6432887e560e4ea3370e578f
J.clinton  Amose <smartamose333@gmail.com>
2023-01-06 19:53:33 +0000

    CURD Using FORM Model

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Room
+from .forms import RoomForm
 
 # rooms = [
 #     { 'id':1,'name':'Learn C'},
@@ -17,3 +18,36 @@ def room(request,id):
     room = Room.objects.get(id=id)
     context={'room':room }
     return render(request,'base/room.html',context)
+
+def createRoom(request):
+    form = RoomForm()
+    context={ 'form': form }
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return  redirect('home')
+            
+    return render(request,'base/room_form.html',context)
+
+def updateRoom(request,id):
+    
+   room = Room.objects.get(id=id)
+   form = RoomForm(instance=room)
+   context={ 'form' : form }
+   if request.method == "POST":
+        form = RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return  redirect('home')
+               
+   return render(request,'base/room_form.html',context)
+
+def deleteRoom(request,id):
+    
+   room = Room.objects.get(id=id)
+   if request.method == "POST":
+        room.delete()
+        return  redirect('home')
+                           
+   return render(request,'base/delete.html',{ 'obj' :room})
