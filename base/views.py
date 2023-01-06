130270a02b2793ac4dfc1ddaae3fdd2599159e2e
J.clinton  Amose <smartamose333@gmail.com>
2023-01-06 01:45:36 +0000

    Setting Templates

--- base/views.py
+++ base/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 
+
+rooms = [
+    { 'id':1,'name':'Learn C'},
+    { 'id':2,'name':'Learn Pyhon' },
+    { 'id':3,'name':'Learn Java' }
+]
+
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    context={'rooms':rooms }
+    return render(request,'base/home.html',context)
 
-def room(request):
-      return render(request,'room.html')
+def room(request,id):
+    room=None
+    for i in rooms:
+      if i['id'] == int(id):
+        room = i
+    context={'room':room }
+    return render(request,'base/room.html',context)
